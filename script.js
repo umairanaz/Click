9bb70261a38ac3c87b2b87841b55b6f460c5e4c7
umairanaz <umairanaz668@gmail.com>
2024-11-27 02:31:07 +0500

    clickme

---
+++ script.js
@@ -0,0 +1,12 @@
+
+var btn = document.querySelector("button");
+var paragraph = document.querySelector(".paragraph")
+
+btn.addEventListener("click", function (e) {
+  paragraph.style.backgroundColor = "lightblue";
+  paragraph.style.fontSize = "30px";
+  paragraph.style.fontStyle = "italic";
+  paragraph.style.fontFamily = "Arial";
+
+
+});
